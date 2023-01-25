ca67b8b76d9b26e3adb7a2a261421e358ad9534e
rajinkhatri <61622498+rajinkhatri@users.noreply.github.com>
2023-01-25 21:13:22 +0545

    made the layout for 2 sections of the page

--- webpages/src/components/Images.jsx
+++ webpages/src/components/Images.jsx
@@ -1,8 +1,22 @@
+import './Images.css';
+
 const Images = () => {
-    return <div>
-        <h1>
-            Images Page
-        </h1>
+    return <div className="Images">
+        <h1 className='header1'>Some images of tuberculosis</h1>
+        <div className="Top-bar">
+            <div className="image-holder">placeholder for image</div>
+            <div className="image-holder">placeholder for image</div>
+            <div className="image-holder">placeholder for image</div>
+            <div className="image-holder">placeholder for image</div>
+        </div>
+
+        <h1 className='header1'>Some images of Pneumonia</h1>
+        <div className="Lower-bar">
+        <div className="image-holder">placeholder for image</div>
+        <div className="image-holder">placeholder for image</div>
+        <div className="image-holder">placeholder for image</div>
+        <div className="image-holder">placeholder for image</div>
+        </div>
     </div>
 }
 
