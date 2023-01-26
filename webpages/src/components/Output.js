61d55626553f3da836226f7360b053a5e63be221
rajinkhatri <61622498+rajinkhatri@users.noreply.github.com>
2023-01-26 20:56:19 +0545

    made the 1st design of output page

--- webpages/src/components/Output.js
+++ webpages/src/components/Output.js
@@ -1,9 +1,26 @@
-function Output(){
-    return <div>
-        <h1>
-            Output Page
-        </h1>
+import './Output.css'
+
+const Output = () => {
+    return <div className="Output">
+        <div className='header-output'>
+        <h1> Results from our model</h1>
+        </div>
+
+        <div className='header-bottom'>
+        <div className='bot-left'>
+            <div className='bot-image'>
+                placeholder for image
+            </div>
+            <div className='bot-predict'>
+                <p>our prediction</p>
+            </div>
+        </div>
+
+        <div className='bot-right'>
+            there will be a table here.
+        </div>
+        </div>
     </div>
 }
 
-export default Output
+export default Output;
