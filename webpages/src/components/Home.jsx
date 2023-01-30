e67f0afcf45677f8a019633ac156345938552bfe
rajinkhatri <61622498+rajinkhatri@users.noreply.github.com>
2023-01-30 23:59:27 +0545

    remade the page using components, the image can now be browsed using the image

--- webpages/src/components/Home.jsx
+++ webpages/src/components/Home.jsx
@@ -1,50 +1,98 @@
-import React from 'react';
+import React, { useState }from 'react';
 import './Home.css';
 import lungs from '../components/Photos';
 
 const Home = () => {
     return (
         <div className="Home-page">
-            <div className='Left-part'>
-                <div className='App-name'>App Name</div>
-                <div className='Info'>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Suspendisse bibendum ex nec quam commodo auctor.
-                        Ut at libero faucibus lacus blandit dictum.
-                        Cras justo leo, congue quis nisl a,
-                        commodo posuere turpis. Nam sit amet tellus non tortor
-                        lobortis mattis nec quis magna.
-                        Duis ligula quam, mollis et feugiat eu.
-                    </p>
-                </div>
-                <div className='Info'><p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Suspendisse bibendum ex nec quam commodo auctor.
-                    Ut at libero faucibus lacus blandit dictum.
-                    Cras justo leo, congue quis nisl a,
-                    commodo posuere turpis. Nam sit amet tellus non tortor
-                    lobortis mattis nec quis magna.
-                    Duis ligula quam, mollis et feugiat eu.
-                </p></div>
-            </div>
-            <div className='Middle-part'>
-                <img src={lungs} className='mid-image' alt='imageofxray'/>
-            </div>
-            <div className='Right-part'>
-                <div className='place-holder'>
-                    placeholder for image
-                </div>
-                <div className='upload-button'>
-                    <button> Submit </button>
-                </div>
-                <div className='upload-text'>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                        Est quod nisi accusantium impedit voluptatibus nesciunt.</p>
-                </div>
-            </div>
+            <LeftPart />
+            <MiddlePart />
+            <RightPart />
         </div>
     )
 }
+
+const LeftPart = () => {
+    return (
+        <div className='Left-part'>
+            <div className='App-name'>App Name</div>
+            <InfoText />
+            <InfoText />
+        </div>
+    )
+}
+
+const InfoText = () => {
+    return (
+        <div className='Info'>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Suspendisse bibendum ex nec quam commodo auctor.
+                Ut at libero faucibus lacus blandit dictum.
+                Cras justo leo, congue quis nisl a,
+                commodo posuere turpis. Nam sit amet tellus non tortor
+                lobortis mattis nec quis magna.
+                Duis ligula quam, mollis et feugiat eu.
+            </p>
+        </div>
+    )
+}
+
+const MiddlePart = () => {
+    return (
+        <div className='Middle-part'>
+            <img src={lungs} className='mid-image' alt='imageofxray'/>
+        </div>
+    )
+}
+
+const RightPart = () => {
+    const [image, setImage] = useState(null);
+
+    const browseImage = () => {
+        const input = document.createElement("input");
+        input.type = "file";
+        input.accept = "image/*";
+        input.onchange = (event) => {
+            setImage(URL.createObjectURL(event.target.files[0]));
+        };
+        input.click();
+    };
+
+    const submitImage = () => {
+        return(
+            alert("image will be submitted")
+        )
+    };
+
+    return (
+        <div className='Right-part'>
+            <div className='place-holder'>
+                <button onClick={browseImage}>
+                    {image ? (
+                        <img src={image} className='mid-image' alt='imageofxray' />
+                    ) : (
+                        <img
+                            src={require("../images/lungs.png")}
+                            className='mid-image'
+                            alt='imageofxray'
+                        />
+                    )}
+                </button>
+            </div>
+            <div className='upload-button'>
+                <button onClick={submitImage}>Submit</button>
+            </div>
+            <div className='upload-text'>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Est quod nisi accusantium impedit voluptatibus nesciunt.
+                </p>
+            </div>
+        </div>
+    );
+};
+
+
 
 export default Home
