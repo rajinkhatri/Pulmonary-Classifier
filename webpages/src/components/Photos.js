46b897bb14277e1cc51ce3c5a7dbb9eaf3aaf51c
rajinkhatri <61622498+rajinkhatri@users.noreply.github.com>
2023-01-31 19:55:23 +0545

    importing of images has been changed, multiple image imported

--- webpages/src/components/Photos.js
+++ webpages/src/components/Photos.js
@@ -1,6 +1,26 @@
 import lungs from '../images/newlungs.png';
-// import appicon from '../images/appicoon.gif';
-// // import bglungs from '../images/middle-image.jpg';
-// import middle from '../images/middle-image.png';
+import pnemo1 from '../images/Pnemo/pnemo1.png'
+import pnemo2 from '../images/Pnemo/pnemo2.png'
+import pnemo3 from '../images/Pnemo/pnemo3.png'
+import pnemo4 from '../images/Pnemo/pnemo4.png'
+import pnemo5 from '../images/Pnemo/pnemo5.png'
+import Tuber1 from '../images/Tuber/Tuber1.png'
+import Tuber2 from '../images/Tuber/Tuber2.png'
+import Tuber3 from '../images/Tuber/Tuber3.png'
+import Tuber4 from '../images/Tuber/Tuber4.png'
+import Tuber5 from '../images/Tuber/Tuber5.png'
 
-export default lungs;
+
+export const images = {
+    lungs,
+    pnemo1,
+    pnemo2,
+    pnemo3,
+    pnemo4,
+    pnemo5,
+    Tuber1,
+    Tuber2,
+    Tuber3,
+    Tuber4,
+    Tuber5
+};
