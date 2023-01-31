bfea0482658c55f373b9f0189a4ab0586260a76e
rajinkhatri <61622498+rajinkhatri@users.noreply.github.com>
2023-01-31 19:52:16 +0545

    added images of tuberculosis and pneumonia

--- webpages/src/components/Images.jsx
+++ webpages/src/components/Images.jsx
@@ -1,25 +1,48 @@
 import './Images.css';
+import React from 'react';
+import {images} from './Photos'
 
 const Images = () => {
-    return <div className="Images">
-        <h1 className='header1'>Some images of tuberculosis</h1>
-        <div className="Top-bar">
-            <div className="image-holder">placeholder for image</div>
-            <div className="image-holder">placeholder for image</div>
-            <div className="image-holder">placeholder for image</div>
-            <div className="image-holder">placeholder for image</div>
-            <div className="image-holder">placeholder for image</div>
+    return(
+        <div className='Images'>
+            <h1 className='header1'>Some images of tuberculosis</h1>
+            <Topbar images={images}/>
+            <h1 className='header1'>Some images of Pneumonia</h1>
+            <Lowbar images={images}/>
         </div>
+    )
+}
 
-        <h1 className='header1'>Some images of Pneumonia</h1>
-        <div className="Lower-bar">
-        <div className="image-holder">placeholder for image</div>
-        <div className="image-holder">placeholder for image</div>
-        <div className="image-holder">placeholder for image</div>
-        <div className="image-holder">placeholder for image</div>
-        <div className="image-holder">placeholder for image</div>
+const Topbar = (props) => {
+    return(
+        <div className='Top-bar'>
+            <Imageholder src={props.images.pnemo1}/>
+            <Imageholder src={props.images.pnemo2}/>
+            <Imageholder src={props.images.pnemo3}/>
+            <Imageholder src={props.images.pnemo4}/>
+            <Imageholder src={props.images.pnemo5}/>
         </div>
-    </div>
+    )
+}
+
+const Imageholder = (props) => {
+    return(
+        <div className='image-holder'>
+        <img src={props.src} alt='drisya of lungs'></img>
+        </div>
+    )
+}
+
+const Lowbar = (props) => {
+    return(
+        <div className='Lower-bar'>
+        <Imageholder src={props.images.Tuber1}/>
+        <Imageholder src={props.images.Tuber2}/>
+        <Imageholder src={props.images.Tuber3}/>
+        <Imageholder src={props.images.Tuber4}/>
+        <Imageholder src={props.images.Tuber5}/>
+        </div>
+    )
 }
 
 export default Images
