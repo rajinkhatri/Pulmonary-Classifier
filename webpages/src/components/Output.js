3f76435c719a60ed3f9c991f2f249fa4fe513ffe
rajinkhatri <61622498+rajinkhatri@users.noreply.github.com>
2023-02-09 09:02:40 +0545

    updated the table section

--- webpages/src/components/Output.js
+++ webpages/src/components/Output.js
@@ -9,7 +9,7 @@ const Output = () => {
         <div className='header-bottom'>
         <div className='bot-left'>
             <div className='bot-image'>
-                placeholder for image
+                <img alt='uploaded by user'></img>
             </div>
             <div className='bot-predict'>
                 <p>our prediction</p>
@@ -17,10 +17,54 @@ const Output = () => {
         </div>
 
         <div className='bot-right'>
-            there will be a table here.
+            {/* <button onClick={showTable} >show table</button> */}
+            <div className='table'>
+            <tr>
+                <td>  </td>
+                <td>Model</td>
+                <td>Resnet50</td>
+                <td>Inception-v3</td>
+            </tr>
+            <tr>
+                <td>accuracy</td>
+                <td>score 1</td>
+                <td>score 2</td>
+                <td>score 3</td>
+            </tr>
+            <tr>
+            <td>precision</td>
+            <td>score 1</td>
+            <td>score 2</td>
+            <td>score 3</td>
+            </tr>
+            <tr>
+            <td>Recall</td>
+            <td>score 1</td>
+            <td>score 2</td>
+            <td>score 3</td>
+            </tr>
+            <tr>
+            <td>F1-score</td>
+            <td>score 1</td>
+            <td>score 2</td>
+            <td>score 3</td>
+            </tr>
+        </div>
         </div>
         </div>
     </div>
 }
+
+// const showTable= () =>{
+//     return(
+//         <div className='table'>
+//             <tr>
+//                 <td>Model</td>
+//                 <td>Resnet50</td>
+//                 <td>Inception-v3</td>
+//             </tr>
+//         </div>
+//     )
+// }
 
 export default Output;
