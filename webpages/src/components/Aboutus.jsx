c5d0e3893226670b76157994cbca7615a69e7211
rajinkhatri <61622498+rajinkhatri@users.noreply.github.com>
2023-02-09 09:01:45 +0545

    added student details and teachers details

--- webpages/src/components/Aboutus.jsx
+++ webpages/src/components/Aboutus.jsx
@@ -1,3 +1,6 @@
+import './Aboutus.css'
+import {images} from '../components/Photos'
+
 const Aboutus= () => {
     return (
         <div className="Abooutus">
@@ -15,32 +18,36 @@ const Aboutus= () => {
 }
 
 const teachers = [
-    {Name: 'Sir', Rollno:5}
+    {Name: 'Sir',
+    Rollno:5,
+    link:'https://www.linkedin.com/in/rajin-khatri-220b49217/'}
 ]
 
 const students = [
-    {Name: 'Rajin', Rollno:1},
-    {Name: 'Sujan', Rollno:2},
-    {Name: 'Amit', Rollno:3},
-    {Name: 'Pilot', Rollno:4}
+    {Name: 'Rajin', Rollno:1, link:'https://www.linkedin.com/in/rajin-khatri-220b49217/'},
+    {Name: 'Sujan', Rollno:2, link:'https://www.linkedin.com/in/sujan-lamichhane-903ba5187/'},
+    {Name: 'Amit', Rollno:3, link:'https://www.facebook.com/97m.it'},
+    {Name: 'Pilot', Rollno:4, link:'https://www.facebook.com/khadka314'}
 ]
 
 const Informationtt = (props) => {
     return(
-        <div>
-            {/* <img src="./" alt="${students.Name}"/> */}
+        <div className='teacher-section'>
+            <img src={images.dp} alt="describing profile"/>
             <p>{props.teachers.Name}</p>
             <p>{props.teachers.Rollno}</p>
+            <a target="_blank" href={props.teachers.link}>Linked in profile</a>
         </div>
     )
 }
 
 const Informationst = (props) => {
     return(
-        <div>
-            {/* <img src="./" alt="${students.Name}"/> */}
+        <div className='student-section'>
+            <img src={images.dp} alt="student profile"/>
             <p>{props.students.Name}</p>
             <p>{props.students.Rollno}</p>
+            <a target='_blank' href={props.students.link}>Linked in</a>
         </div>
     )
 }
