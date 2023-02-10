6db9b3684b87a6d2a5512a343896cfb8bd01a787
rajinkhatri <61622498+rajinkhatri@users.noreply.github.com>
2023-02-10 22:09:18 +0545

    Made the necessary changes to issue #1

--- webpages/src/components/Output.js
+++ webpages/src/components/Output.js
@@ -12,59 +12,11 @@ const Output = () => {
                 <img alt='uploaded by user'></img>
             </div>
             <div className='bot-predict'>
-                <p>our prediction</p>
+                <p>our prediction: </p>
             </div>
-        </div>
-
-        <div className='bot-right'>
-            {/* <button onClick={showTable} >show table</button> */}
-            <div className='table'>
-            <tr>
-                <td>  </td>
-                <td>Model</td>
-                <td>Resnet50</td>
-                <td>Inception-v3</td>
-            </tr>
-            <tr>
-                <td>accuracy</td>
-                <td>score 1</td>
-                <td>score 2</td>
-                <td>score 3</td>
-            </tr>
-            <tr>
-            <td>precision</td>
-            <td>score 1</td>
-            <td>score 2</td>
-            <td>score 3</td>
-            </tr>
-            <tr>
-            <td>Recall</td>
-            <td>score 1</td>
-            <td>score 2</td>
-            <td>score 3</td>
-            </tr>
-            <tr>
-            <td>F1-score</td>
-            <td>score 1</td>
-            <td>score 2</td>
-            <td>score 3</td>
-            </tr>
-        </div>
         </div>
         </div>
     </div>
 }
-
-// const showTable= () =>{
-//     return(
-//         <div className='table'>
-//             <tr>
-//                 <td>Model</td>
-//                 <td>Resnet50</td>
-//                 <td>Inception-v3</td>
-//             </tr>
-//         </div>
-//     )
-// }
 
 export default Output;
